09fd7a52589a96efab5978ecb8a61f5d4a8f1381
forge-admin <github@forgethepath.com>
2018-01-06 23:49:16 -0700

    added basic search to page

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,39 @@
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
+
 const search = instantsearch({
   appId: '8NN8HMRB8K',
   apiKey: 'c307b2769e49beebfebf51ed261b0d8b',
-  urlSync: true,
-  searchParameters: {
-    hitsPerPage: 10
-  }
+  indexName: 'forge_search'
 });
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+    placeholder: 'Search for Posts'
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits-container',
+    templates: {
+      item: 'Hit {{objectID}}: {{title}}'
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination-container'
+  })
+);
+
+search.start();
