7a3676bda1289090ff349aa3d03961a9f1507991
Forge <github@forgethepath.com>
2018-02-24 22:40:35 -0700

    wrapped scroll bar content shifting fix in media query

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,29 @@ if (window.netlifyIdentity) {
   })
 };
 
+// const browser_html = document.querySelector("html");
+// const nav_offset = document.querySelector(".forge-navigation ul");
+// const has_scroll = (window.innerWidth > document.documentElement.clientWidth);
+// const scroll_offset = (window.innerWidth - document.documentElement.clientWidth);
+
+// // const viewport_width = window.innerWidth;
+// // const DOM_width = browser_html.getBoundingClientRect().width;
+// // const scroll_offset = (viewport_width - DOM_width);
+
+// const mq = window.matchMedia('(max-width: 700px)');
+// const lg_screen = console.log("greater then 700px");
+// const sm_screen = console.log("less then 700px");
+
+// mq.addListener((changed) => {
+//   changed.matches? lg_screen : sm_screen
+// });
+
+// if (!has_scroll) {
+//   console.log("browser has scroll bar")
+//   // browser_html.style.marginLeft = "calc(100vw - 100%)"
+
+// }
+
 const newsletter = document.getElementById("forge-news");
 newsletter.addEventListener("submit", (event) => {  
   let form = event.target;
